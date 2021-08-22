4ebcc0dd0aa9d039f7594f2c0b9864d580ac64b8
reverzz <66375293+reverzz@users.noreply.github.com>
2021-08-22 22:14:20 +0500

    2 module: 1 task completed
    Реализована функция вычисления суммы зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let key in salaries) {
+    if (!Number.isNaN(salaries[key]) && Number.isFinite(salaries[key])) {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
