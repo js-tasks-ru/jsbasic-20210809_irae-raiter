968ff829b89d3c0464014497c5afcd35ef3c682b
reverzz <66375293+reverzz@users.noreply.github.com>
2021-09-04 22:39:26 +0500

    5 module: 2 task completed
    Реализована функция скрытия и повторного показа элемента при нажатии на кнопку

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  const element = document.querySelector('#text');
+  button.addEventListener('click', () => element.hidden = !element.hidden);
 }
